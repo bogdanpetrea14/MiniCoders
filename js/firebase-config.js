3b0016088a2a57e80007c26435afe13205ac18e0
Alexia Enache <alexia_ioana.enache@stud.acs.upb.ro>
2025-12-17 21:27:14 +0200

    Add Firestore import to firebase-config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
